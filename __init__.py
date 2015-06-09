935fdbe5feea61f183797085cc906f16ff5afb98
Rolf Sievers <rosievers@googlemail.com>
2015-06-09 10:45:45 +0200

    Functional containers, automated item management.

--- __init__.py
+++ __init__.py
@@ -1,18 +1,32 @@
 import pygame
 import color
 
+from random import randrange
+
 class Window(object):
     """bigPixelPlacer.Window(..) can be created once and is used to interact with the game Window"""
-    def __init__(self, pixelSize, width, height,
+    def __init__(self, pixelSize=1, width=None, height=None,
                  caption="Big Pixel Placer - Unnamed Project",
-                 fps=30):
+                 fps=30,
+                 content=None):
         pygame.init()
 
         self.pixelSize = pixelSize
+
+        if content is not None:
+            if width is not None:
+                raise "The arguments 'width' and 'content' are incompatible"
+            if height is not None:
+                raise "The arguments 'height' and 'content' are incompatible"
+            width = content.size.x
+            height = content.size.y
         self.width = width
         self.height = height
-
-        self.content = DataContainer(width, height, color.white, self.updatePixel)
+        if content is None:
+            self.content = DataContainer(width, height, color.white, self.updatePixel)
+        else:
+            self.content = content
+            self.content.hook = self.updatePixel
 
         self.refresh_display_size()
         self.caption = caption
@@ -54,7 +68,6 @@ class Window(object):
         for e in pygame.event.get():
             yield e
 
-
 class DataContainer(object):
     """This class embodies a 2-dimensional array which contains arbitrary data"""
     def __init__(self, width, height, initialValue=None, hook=None):
@@ -76,6 +89,86 @@ class DataContainer(object):
         if self.hook is not None:
             value = self.hook(key, value)
         self.raw[key.x][key.y] = value
+    @property
+    def randomLocation(self):
+        return Vector(randrange(self.size.x), randrange(self.size.y))
+
+class MapContainer(object):
+    """This virtual read-only container class can present the content of
+    another container in a nicer way"""
+    def __init__(self, baseContainer, fkt, hook=None):
+        self.baseContainer = baseContainer
+        self.fkt = fkt
+        self.hook = hook
+        self.baseContainer.hook = self.hookFunction
+    @property
+    def size(self):
+        return self.baseContainer.size
+    def hookFunction(self, key, value):
+        if self.hook is not None:
+            self.hook(key, self.fkt(value))
+            return value
+        return value
+    def allPositions(self):
+        return self.baseContainer.allPositions()
+    def __len__(self):
+        return len(self.baseContainer)
+    def __getitem__(self, key):
+        return self.fkt(self.baseContainer[key])
+    def randomLocation(self):
+        return self.baseContainer.randomLocation()
+
+class AutomatedItemStock(object):
+    def __init__(self, item, container, minimalStock=None, maximalStock=None):
+        self.item = item
+        self.container = container
+        self.minimalStock = minimalStock
+        self.maximalStock = maximalStock
+        self.positions = []
+    def placeRandom(self, tries=1):
+        while tries > 0:
+            tries -= 1
+            position = self.container.randomLocation
+            if self.maybePlaceAt(position) is not None:
+                return position
+        return None
+    def maybePlaceAt(self, position):
+        try:
+            return self.placeAt(position)
+        except:
+            return None
+    def placeAt(self, position):
+        if self.container[position] is None:
+            self.container[position] = self.item
+            self.positions.append(position)
+            self.cullStock()
+            return position
+        else:
+            raise "Position is already occupied!"
+    def removeAt(self, position):
+        removalIndex = self.positions.index(position)
+        self.removeIndex(removalIndex)
+    def removeIndex(self, removalIndex):
+        position = self.positions.pop(removalIndex)
+        self.container[position] = None
+        self.breedStock()
+
+    def cullStock(self):
+        if self.maximalStock is None:
+            return
+        while self.maximalStock < len(self.positions):
+            self.removeIndex(0)
+    def breedStock(self, tries=1):
+        if self.minimalStock is None:
+            return
+        while self.minimalStock > len(self.positions):
+            if self.placeRandom(tries=tries) is None:
+                # if placing fails, fail.
+                return None
+    def tendStock(self, tries=1):
+        self.cullStock()
+        self.breedStock(tries=tries)
+
 
 class Vector(object):
     """A 2-dimensional vector class."""
@@ -97,6 +190,8 @@ class Vector(object):
         return Vector(self.x % other.x, self.y % other.y)
     def __str__(self):
         return "(%s, %s)" % (self.x, self.y)
+    def __repr__(self):
+        return self.__str__()
 
 class Matrix(object):
     """A 2x2-dimensional matrix, data input in row form.
--- color.py
+++ color.py
@@ -2,3 +2,5 @@ import pygame.color
 
 white = pygame.color.Color("white")
 black = pygame.color.Color("black")
+red = pygame.color.Color("red")
+blue = pygame.color.Color("blue")
